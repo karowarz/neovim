a63a1aaa20761705c2083f98b005a64dbd24c9e3
Warzecha Karol <karol.warzecha@unic.com>
2023-03-27 20:02:28 +0200

    add logsitter plugin for fast console.log

---
+++ lua/plugins/logsitter.lua
@@ -0,0 +1,11 @@
+return {
+  "gaelph/logsitter.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  keys = {
+    {
+      "<leader>ol",
+      ":Logsitter<cr>",
+      desc = "consol.log()",
+    },
+  },
+}
