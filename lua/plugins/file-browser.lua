8a26ed16bb65baf6fae8ee29e3b5f66845dfcfb8
Warzecha Karol <karol.warzecha@unic.com>
2023-05-30 16:19:56 +0200

    add Oil File Manager

--- lua/plugins/file-browser.lua
+++ lua/plugins/file-browser.lua
@@ -1,11 +1,11 @@
 return {
   "nvim-telescope/telescope-file-browser.nvim",
   keys = {
-    {
-      "<leader>;",
-      ":Telescope file_browser path=%:p:h=%:p:h<cr>",
-      desc = "Browse File",
-    },
+    -- {
+    --   "<leader>;",
+    --   ":Telescope file_browser path=%:p:h=%:p:h<cr>",
+    --   desc = "Browse File",
+    -- },
   },
   config = function()
     require("telescope").load_extension("file_browser")
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {},
+  keys = {
+    {
+      "<leader>;",
+      ":Oil --float<CR>",
+      desc = "Oil File",
+    },
+  },
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+}
