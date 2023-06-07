5a19965cd742876edc7797d8b665447660ecdc7f
Warzecha Karol <karol.warzecha@unic.com>
2023-06-07 10:26:24 +0200

    add egdy plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,14 +2,20 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+
 map("n", "-", require("oil").open, { desc = "Open parent directory" })
-map("n", "<cr>", "ciw", { desc = "Change inside word" })
 map("v", "y", "ygv<esc>", { desc = "Copy and put cursor at the end" })
+map("n", "<cr>", "ciw", { desc = "Change inside word" })
 map(
   "n",
   ";",
   "<cmd>lua require('telescope.builtin').resume(require('telescope.themes').get_ivy({}))<cr>",
   { desc = "Browse recent files" }
 )
-map("v", "J", ":m '>+1<CR>gv==kgvo<esc>=kgvo", { desc = "move highlighted text down" })
-map("v", "K", ":m '<-2<CR>gv==jgvo<esc>=jgvo", { desc = "move highlighted text up" })
+-- map("v", "J", ":m '>+1<CR>gv==kgvo<esc>=kgvo", { desc = "move highlighted text down" })
+-- map("v", "K", ":m '<-2<CR>gv==jgvo<esc>=jgvo", { desc = "move highlighted text up" })
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<leader>wh", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<leader>wj", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<leader>wl", "<C-w>l", { desc = "Go to right window", remap = true })
+map("n", "<leader>wk", "<C-w>k", { desc = "Go to upper window", remap = true })
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.ui.edgy" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
