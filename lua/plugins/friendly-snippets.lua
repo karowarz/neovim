bccc3137e3f80eee899680ef9e9d4f5d6b52f201
Warzecha Karol <karol.warzecha@unic.com>
2023-06-15 17:43:03 +0200

    refactor: :zap: add js snippets to different enviroments

--- lua/plugins/friendly-snippets.lua
+++ lua/plugins/friendly-snippets.lua
@@ -2,9 +2,9 @@ return {
   "rafamadriz/friendly-snippets",
   config = function()
     local luasnip = require("luasnip")
-    -- luasnip.filetype_extend("javascript", { "javascriptreact" })
-    -- luasnip.filetype_extend("javascriptreact", { "javascriptreact" })
-    -- luasnip.filetype_extend("typescriptreact", { "javascriptreact" })
+    luasnip.filetype_extend("javascript", { "javascriptreact" })
+    luasnip.filetype_extend("javascriptreact", { "javascriptreact" })
+    luasnip.filetype_extend("typescriptreact", { "javascriptreact" })
 
     require("luasnip/loaders/from_vscode").lazy_load()
   end,
